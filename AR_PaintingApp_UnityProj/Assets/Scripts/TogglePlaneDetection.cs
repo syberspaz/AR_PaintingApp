00d6637d05eb627167c9edaa3d4397e710bee654
Noah Glassford <noahglassford@gmail.com>
2021-05-04 14:08:32 -0700

    Basic Image Tracking
    In order to test, use the images in the asset folder. There are some issues tracking 2 images at once, and once an image is tracked it seems to lock on it.

---
+++ AR_PaintingApp_UnityProj/Assets/Scripts/TogglePlaneDetection.cs
@@ -0,0 +1,54 @@
+//This script was taken from:
+//https://youtu.be/1Edsroa16fo
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARPlaneManager))]
+public class TogglePlaneDetection : MonoBehaviour
+{
+    private ARPlaneManager planeManager;
+
+    [SerializeField]
+    private Text toggleButtonText;
+
+    private void Awake()
+    {
+        planeManager = GetComponent<ARPlaneManager>();
+
+        toggleButtonText.text = "Disable";
+    }
+
+    public void PlaneDetectionToggle()
+    {
+        planeManager.enabled = !planeManager.enabled;
+        string toggleButtonMessage = "";
+
+        if (planeManager.enabled)
+        {
+            toggleButtonMessage = "Disable";
+            SetAllPlannesActive(true);
+        }
+        else
+        {
+            toggleButtonMessage = "Enable";
+            SetAllPlannesActive(false);
+        }
+
+        toggleButtonText.text = toggleButtonMessage;
+    }
+
+    private void SetAllPlannesActive(bool value)
+    {
+        foreach(var plane in planeManager.trackables)
+        {
+            plane.gameObject.SetActive(value);
+        }
+    }
+
+
+}
