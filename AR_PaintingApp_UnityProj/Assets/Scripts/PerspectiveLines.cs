8b63884e3209fbd24e308ef00d0fec5644904ef1
Noah Glassford <noahglassford@gmail.com>
2021-05-06 12:51:12 -0400

    Added controls to the perspective lines, can now create more than 1
    Added many control features to the perspective lines, such as a custom material, sliders to control all aspects of them (rotation per line, start and end position) and a way to destroy and create lines with 1 function

--- AR_PaintingApp_UnityProj/Assets/Scripts/PerspectiveLines.cs
+++ AR_PaintingApp_UnityProj/Assets/Scripts/PerspectiveLines.cs
@@ -4,49 +4,98 @@ using UnityEngine;
 
 public class PerspectiveLines : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Vector3 start = new Vector3(0, 0, 0);
-        Vector3 end = new Vector3(10, 0, 0);
-        Color col = Color.blue;
+    [SerializeField]
+    private Material LineMaterial;
 
+    [SerializeField]
+    public Vector3 start = new Vector3(0, 0, 0);
+    [SerializeField]
+    public Vector3 end = new Vector3(10, 0, 0);
+    [SerializeField]
+    public Color col = Color.blue;
+    [SerializeField]
+    public int NumOfLines;
+    [SerializeField]
+    public Vector3 Rotation = new Vector3(10,10,10);
 
-
-        for (int i = 0; i < 100; i++)
-        {
-            DrawLine(start, end, col, 100.0f);
-            end = RotatePointAroundPivot(end, start, new Vector3(10, 10, 10));
-        }
-      
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
-
-    void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.2f)
+    void DrawLine(Vector3 start, Vector3 end, Color color)
     {
         GameObject myLine = new GameObject();
+        myLine.gameObject.tag = "PerspectiveLine";
         myLine.transform.position = start;
         myLine.AddComponent<LineRenderer>();
         LineRenderer lr = myLine.GetComponent<LineRenderer>();
-        lr.material = new Material(Shader.Find("Standard"));
+        lr.material = LineMaterial;
         lr.startColor = color;
         lr.endColor = color;
         lr.startWidth = 0.01f;
         lr.endWidth = 0.01f;
         lr.SetPosition(0, start);
         lr.SetPosition(1, end);
-        GameObject.Destroy(myLine, duration);
+
+
     }
 
-    public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
+    public void CreateLines(int NumOfLines)
+    {
+        for (int i = 0; i < NumOfLines; i++)
+        {
+            DrawLine(start, end, col);
+            end = RotatePointAroundPivot(end, start, Rotation);
+        }
+    }
+
+    private Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
     {
         return Quaternion.Euler(angles) * (point - pivot) + pivot;
     }
 
+    public void DestroyAllLines()
+    {
+       GameObject[] PerspectiveLines = GameObject.FindGameObjectsWithTag("PerspectiveLine");
+
+        for(int i = 0; i < PerspectiveLines.Length; i++)
+        {
+            GameObject.Destroy(PerspectiveLines[i], 0.0f);
+        }
+
+    }
+
+    public void SetRotX(float rot)
+    {
+        Rotation.x = rot;
+    }
+    public void SetRotY(float rot)
+    {
+        Rotation.y = rot;
+    }
+    public void SetRotZ(float rot)
+    {
+        Rotation.z = rot;
+    }
+    public void SetStartPosX(float pos)
+    {
+        start.x = pos;
+    }
+    public void SetStartPosY(float pos)
+    {
+        start.y = pos;
+    }
+    public void SetStartPosZ(float pos)
+    {
+        start.z = pos;
+    }
+    public void SetEndPosX(float pos)
+    {
+        end.x = pos;
+    }
+    public void SetEndPosY(float pos)
+    {
+        end.y = pos;
+    }
+    public void SetEndPosZ(float pos)
+    {
+        end.z = pos;
+    }
 
 }
