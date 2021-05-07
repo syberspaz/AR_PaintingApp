152bc24b919a3539c32701cdf251d9b72fc52f61
Noah Glassford <noahglassford@gmail.com>
2021-05-07 14:40:20 -0400

    Main Menu implemented
    Added a prototype for the main menu, allows access to options, learn mode and explore mode

---
+++ AR_PaintingApp_UnityProj/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void LaunchLearnMode()
+    {
+        //put the code here to switch to learn mode
+    }
+    public void LaunchExploreMode()
+    {
+        //put the code here to switch to explore mode
+    }
+
+
+}
