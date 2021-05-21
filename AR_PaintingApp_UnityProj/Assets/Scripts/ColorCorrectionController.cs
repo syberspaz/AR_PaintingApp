dda9caadfb44251ede432d41d3e387f2a64ff5dc
Noah Glassford <noahglassford@gmail.com>
2021-05-21 16:28:53 -0400

    Added a color picker to the color correction and a way to toggle

--- AR_PaintingApp_UnityProj/Assets/Scripts/ColorCorrectionController.cs
+++ AR_PaintingApp_UnityProj/Assets/Scripts/ColorCorrectionController.cs
@@ -9,18 +9,24 @@ public class ColorCorrectionController : MonoBehaviour
     [Tooltip("This is the global volume")]
     [SerializeField]
     private Volume Volume;
+    public FlexibleColorPicker fcp;
 
-    public void ChangeColor()
+    private bool effectToggle = false;
+
+    public void Update()
     {
         ColorAdjustments colorAdjustments;
 
         Volume.profile.TryGet<ColorAdjustments>(out colorAdjustments);
 
-        colorAdjustments.colorFilter.Override(Color.green); //SetValue(new ColorParameter(Color.blue, true, false, true));
+        if (effectToggle)
+        colorAdjustments.colorFilter.Override(fcp.color);
+        else
+        colorAdjustments.colorFilter.Override(Color.white);
+    }
 
-
-
-
-      
+    public void ToggleEffect()
+    {
+        effectToggle = !effectToggle;
     }
 }
