f165892a0b8ed23fd34e1206889f91f721047c91
Noah Glassford <noah.glassford@ontariotechu.net>
2021-11-22 16:48:02 -0500

    Warning for tracking lost

--- AR_PaintingApp_UnityProj/Assets/Scripts/UI/TrackingStateManager.cs
+++ AR_PaintingApp_UnityProj/Assets/Scripts/UI/TrackingStateManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using UnityEngine.UI;
+
 using TMPro;
 public class TrackingStateManager : MonoBehaviour
 {
